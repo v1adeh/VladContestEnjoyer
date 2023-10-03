3b4cc36cf2036f7e964bfc73ca0860410c55fef8
v1adeh <144953007+v1adeh@users.noreply.github.com>
2023-10-03 09:43:08 +0300

    Create 20.cpp

---
+++ contest_1/20.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a,b;
+    cin >> a >> b;
+    
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    
+    if(a == b) {
+        cout << "YES";
+    }
+    else cout << "NO";
+}
